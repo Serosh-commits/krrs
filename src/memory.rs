78079588ef038e1f6d7804c24fbc325b2f474fa9
Serosh <janmejayapanda400@gmail.com>
2026-01-14 22:03:04 +0530

    Add src/memory.rs

---
+++ src/memory.rs
@@ -0,0 +1,47 @@
+use bootloader::bootinfo::{MemoryMap, MemoryRegionType};
+use x86_64::{
+    registers::control::Cr3,
+    structures::paging::{FrameAllocator, OffsetPageTable, PageTable, PhysFrame, Size4KiB},
+    PhysAddr, VirtAddr,
+};
+
+pub unsafe fn init(offset: VirtAddr) -> OffsetPageTable<'static> {
+    let level4 = active_level_4(offset);
+    OffsetPageTable::new(level4, offset)
+}
+
+unsafe fn active_level_4(offset: VirtAddr) -> &'static mut PageTable {
+    let (frame, _) = Cr3::read();
+    let virt = offset + frame.start_address().as_u64();
+    &mut *(virt.as_mut_ptr())
+}
+
+pub struct BootInfoFrameAllocator {
+    memory_map: &'static MemoryMap,
+    next: usize,
+}
+
+impl BootInfoFrameAllocator {
+    pub unsafe fn new(memory_map: &'static MemoryMap) -> Self {
+        Self {
+            memory_map,
+            next: 0,
+        }
+    }
+
+    fn usable_frames(&self) -> impl Iterator<Item = PhysFrame> {
+        let regions = self.memory_map.iter();
+        let usable_regions = regions.filter(|r| r.region_type == MemoryRegionType::Usable);
+        let addr_ranges = usable_regions.map(|r| r.range.start_addr()..r.range.end_addr());
+        let frame_addresses = addr_ranges.flat_map(|r| r.step_by(4096));
+        frame_addresses.map(|addr| PhysFrame::containing_address(PhysAddr::new(addr)))
+    }
+}
+
+unsafe impl FrameAllocator<Size4KiB> for BootInfoFrameAllocator {
+    fn allocate_frame(&mut self) -> Option<PhysFrame<Size4KiB>> {
+        let frame = self.usable_frames().nth(self.next);
+        self.next += 1;
+        frame
+    }
+}
